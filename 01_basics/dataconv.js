b1877e7c9b0271c592ceb1f896505eea4a92ed36
Farhana Shaik <154665613+Farhanacode@users.noreply.github.com>
2024-03-16 07:42:20 +0000

    conversations and operations

---
+++ 01_basics/dataconv.js
@@ -0,0 +1,30 @@
+let score="33"
+//console.log(typeof score);
+let valueInNumber=Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+
+//"33"--33
+//"33abc"--NaN
+//true--1 ; false--0
+
+let IsLoggedIn=""
+let BooleanIsLoggedIn=Boolean(IsLoggedIn)
+//console.log(BooleanIsLoggedIn);
+
+//1 =>true; 0 =>false
+//""=>false
+//"farhana" =>true
+let number=45
+let StringInnumber=String(number)
+//console.log(StringInnumber);
+//console.log(typeof StringInnumber);
+
+//**************operations*******************
+let value=3
+let negvalue=-value
+//console.log(negvalue);
+let str1="hello"
+let str2=" farhana"
+let str3=str1+str2
+//console.log(str3);
