d1c814a6228bbc52c9249b7542a8ad15fb6b35a4
Farhana Shaik <154665613+Farhanacode@users.noreply.github.com>
2024-03-18 13:27:58 +0000

    conversions and opertions

--- 01_basics/dataconv.js
+++ 01_basics/dataconv.js
@@ -28,3 +28,9 @@ let str1="hello"
 let str2=" farhana"
 let str3=str1+str2
 //console.log(str3);
+//console.log("1"+2+2)
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter);
+//a++ =>postfix(returns same value if we print again it adds upto one)
+//++a =>prefix(adds one and return the value)
