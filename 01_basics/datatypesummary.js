19a7954102343be22073024b5525c04d5fae5f8f
Farhana Shaik <154665613+Farhanacode@users.noreply.github.com>
2024-03-19 04:47:24 +0000

    datatpes summary

---
+++ 01_basics/datatypesummary.js
@@ -0,0 +1,14 @@
+// Non-primitive (reference) datatype
+//arrays,object & function
+const heroes=["ironman","batman","venom"]
+const myobj={
+    name:"farhana",
+    age:23,
+ }
+const myfunction=function(){
+    console.log("hello world");
+}
+console.log(typeof myfunction);
+
+// primitive datatypes
+// 7 types --- string,null,number,bigint,boolean,symbol and undefined
