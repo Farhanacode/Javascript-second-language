b40929fb46eb8cda5408871de068fdc626d8fc38
Farhana Shaik <154665613+Farhanacode@users.noreply.github.com>
2024-03-19 06:51:07 +0000

    number and maths types

---
+++ 01_basics/nums_maths.js
@@ -0,0 +1,29 @@
+//----------------Numbers------------------
+
+const score=400
+//console.log(score);
+
+const value= new Number(134.89)
+//console.log(value.toString().length);
+//console.log(value.toExponential(3))
+//console.log(value);
+//console.log(value.toFixed(3));
+//console.log(value.toPrecision(4))
+const balance=10000000
+//console.log(balance.toLocaleString('en-In'));
+
+
+//-------------------maths----------------------
+// console.log(Math);
+// console.log(Math.abs(-4));//- value to + value
+// console.log(Math.round(4.6));//round off
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(2,5,6,7,1));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+const min=5
+const max=25
+console.log(Math.floor(Math.random()*(max-min+1))+min)
