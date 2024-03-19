d26aa01abe65ccd4506d498f03738fa77787f334
Farhana Shaik <154665613+Farhanacode@users.noreply.github.com>
2024-03-19 06:00:08 +0000

    strings and methods

---
+++ 01_basics/strings.js
@@ -0,0 +1,21 @@
+const name="Farhana"
+const age=23
+//console.log(name + age +" adult");(it is inapprotiate way)
+//console.log(`hello my name is ${name} and my age is ${age} im an adult`);//(it is an proper and modern way to define String)
+const gameName=new String('farhana')
+//console.log(gameName.length);
+// console.log(gameName[3]);
+// console.log(gameName.indexOf('n'));
+// console.log(gameName.charAt(5));
+//console.log(gameName.toLowerCase());
+// console.log(gameName.valueOf());
+console.log(gameName.charCodeAt(1));
+// const newString=gameName.substring(0,4)
+// console.log(newString);
+const newString=gameName.slice(2,7)
+//console.log(newString);
+const newStringone="farru google com"
+console.log(newStringone);
+//console.log(newStringone.trim());
+//console.log(newStringone.replace('u','h'));
+console.log(newStringone.split(' '));
