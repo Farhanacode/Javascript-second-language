710f6a4326be8312cc7d8c0f35af67506e0de321
Farhana Shaik <154665613+Farhanacode@users.noreply.github.com>
2024-03-20 10:01:26 +0000

    arrays

---
+++ 02_basics/arrays.js
@@ -0,0 +1,23 @@
+const myarr=[1,2,3,4,5,6,7]
+//console.log(myarr[3]);
+const myarr2=new Array(1,2,3,4,5,6)
+//console.log(myarr2[3]);
+////////array methods//////////
+// myarr.push(6);
+// console.log(myarr);
+// myarr.pop();
+// console.log(myarr);
+// myarr.unshift(9);
+// console.log(myarr);
+// myarr.shift();
+// console.log(myarr);
+// const newArr=myarr.join()//converts datatype into string
+// console.log(myarr);
+// console.log( typeof newArr)
+console.log("a",myarr)
+const myn1=myarr.slice(1,4)
+console.log(myn1);
+console.log("b",myarr);
+const myn2=myarr.splice(1,4)
+console.log(myn2);
+console.log("c",myarr);
