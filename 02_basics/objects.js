448ac2b354ca6bbc71049f615cb88b2c1d063056
Farhana Shaik <154665613+Farhanacode@users.noreply.github.com>
2024-03-20 11:28:02 +0000

    objects and manipulation

---
+++ 02_basics/objects.js
@@ -0,0 +1,28 @@
+//objects can be defined in two types <=1.literal and 2.construct
+//singleton--definr through construct way
+//object.create
+const mysym=Symbol("grade")
+ const myuser={
+    name:"farhana",
+    "fullname":"farhana Shaik",
+    [mysym]:"key1",
+    age:23,
+    email:"farhana@google.com",
+    city:"gudur"
+ }
+//  console.log(myuser.age);
+//  console.log(myuser["fullname"]);
+//  console.log( myuser[mysym]);
+// console.log(myuser);
+myuser.email="rajesh@google.com"
+// Object.freeze(myuser)
+// myuser.email="farru@google.com"
+//console.log(myuser);
+myuser.greeting=function(){
+    console.log("hello my user");
+}
+myuser.greetingtwo=function(){
+    console.log(`hello user, ${this.fullname}`);
+}
+console.log(myuser.greeting());
+console.log(myuser.greetingtwo());
