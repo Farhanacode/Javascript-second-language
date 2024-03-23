f29e38783ae90bc27370b39a2573354f50a29fba
Farhana Shaik <154665613+Farhanacode@users.noreply.github.com>
2024-03-23 06:02:34 +0000

    functions syntax

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -17,4 +17,27 @@ function loginusername(username="farru"){
 }
 //console.log(loginusername())//undefined
 //console.log(loginusername(""))//empty
-console.log(loginusername("farhana"))
+//console.log(loginusername("farhana"))
+
+function calculateprice(val2,...num1){
+    return num1
+}
+//console.log(calculateprice(200,100,400,200));
+const user={
+    name:"farhana",
+    age:23,
+    price:700
+}
+function handleObject(anyobject){
+    console.log(` customer ${anyobject.name} is the user and the age is${anyobject.age} paid upto ${anyobject.price}`);
+}
+handleObject({
+    name:"farhana",
+    age:23,
+    price:700
+})
+const myarr=[100,200,300,400]
+function returnsecondvalue(getArray){
+    return getArray[1];
+}
+console.log(returnsecondvalue([100,200,300,40]));
