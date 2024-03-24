f9ed695f0415947b11c388869b7ad33a2454ae71
Farhana Shaik <154665613+Farhanacode@users.noreply.github.com>
2024-03-24 18:08:06 +0000

    IIFES

--- 03_basics/iife.js
+++ 03_basics/iife.js
@@ -1,5 +1,6 @@
 //Immediately Invoked function Expressions(IIFE)
 //immediately excuted and avoid polluted of global scope
+// types of iifes (simple,named and parameters passing)
 (function namde(){    //named iife
     console.log(`db connected`);
 })();
