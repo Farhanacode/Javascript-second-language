4e6c9287de98068c13b358b72870d8b757935c3c
Farhana Shaik <154665613+Farhanacode@users.noreply.github.com>
2024-03-28 12:54:07 +0000

    for eachloop(hig order func)

---
+++ itreations/05.js
@@ -0,0 +1,29 @@
+//for each-arrays(high order function)
+const programming=["java","cpp","python","go"]
+programming.forEach(function (items,index,arr){
+    //console.log(items,index,arr);
+})//normal function & multiple arguments pass also happens
+programming.forEach((items) =>{
+//console.log(items);
+})//arrow function
+function name(items){
+//console.log(items);
+}
+programming.forEach(name)//function name
+const coding=[
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+coding.forEach((item) =>{
+    console.log(item.languageName);
+})
