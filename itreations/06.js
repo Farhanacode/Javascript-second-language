cf81826092b4e12e13e394ed7a9a3e3889eb42c9
Farhana Shaik <154665613+Farhanacode@users.noreply.github.com>
2024-03-28 15:22:50 +0000

    filter

---
+++ itreations/06.js
@@ -0,0 +1,23 @@
+//filter
+const mynums=[1,2,3,4,5]
+const ner=mynums.filter((num) =>{ return num>=3})
+//console.log(ner);
+const nenums=[]
+mynums.forEach((num)=>{
+    if(num>=3)
+    nenums.push(num)
+})
+//console.log(nenums);//for each using new variable
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+  const userbk=books.filter((bk)=> bk.genre==='Fiction')
+  console.log(userbk);
