32767e5bb39bbe7cdc24c60e978e2e96dfef6805
Farhana Shaik <154665613+Farhanacode@users.noreply.github.com>
2024-03-28 11:55:10 +0000

    for of loop

---
+++ itreations/03.js
@@ -0,0 +1,20 @@
+//for of
+const arr=[1,2,3,4,5]
+for (const num of arr) {
+   // console.log(num);
+}//arrays
+const greetings="hello guys"
+for (const greet of greetings) {
+    // if(greet==" "){
+    //     continue
+    // }
+   // console.log(`each char is ${greet}`);
+}
+//Maps
+const map=new Map()
+map.set('In',"india")
+map.set('us',"united states")
+//console.log(map);
+for (const [key,value] of map) {
+    console.log(key,':-',value);
+}
